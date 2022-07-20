cb3de2377cbce005c731711886c92c9065467cff
yourk <2109065@upy.edu.mx>
2022-07-20 18:20:27 -0500

    Obstacles and timer made it

--- Pythonpart.py
+++ Pythonpart.py
@@ -9,13 +9,14 @@ import serial, time
 
 pygame.init()
 
-screen = pygame.display.set_mode((700, 700))
+screen = pygame.display.set_mode((900, 900))
 font = pygame.font.Font(None, 30)
 clock = pygame.time.Clock()
 
 def obstacles():
-    random_pos = random.randint(0,49)*10
-    obstacles_places = [490,random_pos]
+    
+    random_pos = random.randint(10,85)*10
+    obstacles_places = [890,random_pos]
     return obstacles_places
 
 def game():
@@ -27,13 +28,26 @@ def game():
     while run:
         
         timer += dt
-        clock.tick(1)
+        clock.tick(10)
+        
+        if timer < 15 :
+            pikes[0] -= 100
+        if timer>15:
+            pikes[0] -=200
+        if pikes[0] <= 0:
+            pikes = obstacles()
+        
         if timer == 60:
          timer = 0
          
+        
+    
+         
         screen.fill((0,0,0))
         text = font.render(str(timer),0,(200,60,80))
-        screen.blit(text, (480,20))
+        
+        pygame.draw.rect(screen,(255,255,255), pygame.Rect(pikes[0], pikes[1], 200, 50))
+        screen.blit(text, (850,120))
         
         
         pygame.display.flip()
