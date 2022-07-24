2ad143aff386db018d2211aebe21222da4fc0e4b
Carlos G <90002059+CarlosGarcia-YK@users.noreply.github.com>
2022-07-24 12:20:07 -0500

    Boarder Colision , buttons and Difficult

--- Pythonpart.py
+++ Pythonpart.py
@@ -1,33 +1,30 @@
-
 """Videogame made by Carlos G and Jair D"""
-from turtle import delay
-from typing import Counter
+
 import pygame, sys, time, random
-from os import system
 from tkinter import*
 import serial, time
 
-#arduino = serial.Serial("COM6", 9600)
+arduino = serial.Serial("COM6", 9600)
 
 pygame.init()
-
+#just colors 
 white=(255,255,255)
 black=(0,0,0)
 red=(255,0,0)
-#Ff
+#Classic variables 
 screen = pygame.display.set_mode((1000, 900))
 font = pygame.font.Font(None, 30)
 clock = pygame.time.Clock()
 
+#Make the fondo and heart 
 Space=pygame.image.load("Fondo.jpg")
-
 pygame.mouse.set_visible(0)
 Heart=pygame.image.load("heart.png")
 Heart.set_colorkey([255,255,255])
 
 #coordenadas del corazon
-coordx=10
-coordy=10
+coordx=450
+coordy=450
 #fps
 speedx= 0
 speedy= 0
@@ -63,7 +60,7 @@ dt = 1
 ap = 0
 press = "Press F to End"
     
-    
+#call the functions by a name 
 pikes = obstacles()
 pikes2 = obstacles2()
 pikes3 = obstacles3()
@@ -75,30 +72,50 @@ while run:
         timer += dt
         clock.tick(60)
         
+        
+        #to make the conter
         if timer>60 :
             realtime+=1
             timer = 0
-        #condition objects 
+        #condition objects based in time
         if realtime< 15 and realtime>-1:
             pikes[0] -= 10
             pikes2[0] -= 10
-            pikes3[0] -= 20
+            pikes3[0] -= 15
             screen.fill((0,0,0))
             modegame = "EASY"  
+        
         if realtime >= 15 and realtime<30:
-            pikes[0] -=10
-            pikes2[0] -=10
-            pikes3[0] -=10
+            pikes[0] -= 15
+            pikes2[0] -= 15
+            pikes3[0] -= 20
+            screen.fill((0,0,0))
+            modegame = "Normal"  
+        
+        if realtime >= 30 and realtime<50:
+            pikes[0] -=20
+            pikes2[0] -=20
+            pikes3[0] -=30
             
             screen.fill((ap,0,0))
             
             modegame = "HARD"
-        if realtime == 30 :
+        if realtime >= 50 and realtime<60:
+            pikes[0] -=35
+            pikes2[0] -=35
+            pikes3[0] -=40
+            
+            screen.fill((ap,0,0))
+            
+            modegame = "TRYHARD"
+        if realtime == 60 :
+            
             pikes[0] = 450
             pikes2[0] = 450
             pikes3[0] = 450
-            realtime = 30
+            realtime = 61
             modegame = "YOU WON"  
+       #to restore the position every time 
         if pikes[0] <= 0:
             pikes = obstacles()
         if pikes2[0] <= 0:
@@ -107,49 +124,60 @@ while run:
             pikes3 = obstacles3()
         
          
+        
+         
+         
+         #Movement when you want in keyboard 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
             
              if event.key== pygame.K_LEFT:
-                speedx=-3
+                speedx=-50
              if event.key==pygame.K_RIGHT:
-                speedx= 3
+                speedx= 50
              if event.key==pygame.K_UP:
-                speedy=-5
+                speedy=-50
              if event.key==pygame.K_DOWN:
-                speedy= 5
-             if event.key == pygame.K_f:
+                speedy= 50
+             if event.key == pygame.K_f: #Just if you want to fail 
                     modegame = "You have failed!"
-                    realtime = 31
+                    realtime = 61
                     print("You has surrendered!")
         
-            if event.type==pygame.KEYUP:
-             if event.key==pygame.K_LEFT:
-                speedx=0
-             if event.key==pygame.K_RIGHT:
-                speedx=0
-             if event.key==pygame.K_UP:
-                speedy=0
-             if event.key==pygame.K_DOWN:
-                speedy=0
-
-        for coord in coor_list:
-         pygame.draw.circle(screen,white, coord,2)
-         coord[1]+=1
-         if coord[1]>700:
-            coord[1]=0
+            
+        x = str(arduino.read().strip()).strip('b')
+        #to move around in bottons 
+        if x == "'1'":
+            speedx=-25
+        if x == "'2'":
+            speedx= 25
+        if x == "'3'":
+            speedy= 25
+        if x == "'4'":
+            speedy= -25
+        if x == "'0'":
+            speedy = 0
+            speedx =0    
         
+        #When the heart hit the boarders 
+        if coordx >= 900:
+            speedx = -10
+        if coordx <= 0:
+            speedx = 10
+        if coordy <= 150:
+            speedy = 10
+        if coordy >= 750:
+            speedy =-10
+        
+        #The special movement 
         coordx+=speedx
         coordy+=speedy   
         
-        if    pikes[1] == coordy and pikes[0] == coordx :
-            
-            press = "Hola"
-            print("Work")
+      
         
        
-        
-        if realtime >=35:
+        #Close when the clock reach 65 seg
+        if realtime >=65:
             run = False
 
 
@@ -162,8 +190,8 @@ while run:
         #Print the objects 
         screen.blit(Space,[0,0])
         pygame.draw.rect(screen,(50,22,22), pygame.Rect(board[0],board[1], 1000,150))
-        pygame.draw.rect(screen,(randomColor,255,255), pygame.Rect(pikes[0], pikes[1], 100, 25))
-        pygame.draw.rect(screen,(255,randomColor,255), pygame.Rect(pikes2[0], pikes2[1], 100, 25))
+        pygame.draw.rect(screen,(randomColor,255,255), pygame.Rect(pikes[0], pikes[1], 200, 75))
+        pygame.draw.rect(screen,(255,randomColor,255), pygame.Rect(pikes2[0], pikes2[1], 100, 50))
         pygame.draw.rect(screen,(255,255,randomColor), pygame.Rect(pikes3[0], pikes3[1], 100, 25))
         text = font.render(str(realtime),0,(200,60,80))
         end = font.render(str(press),0,(200,60,80))
@@ -174,6 +202,39 @@ while run:
         
         screen.blit(Heart,[coordx,coordy])
         pygame.display.flip()
+
+
+
+    
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+           
+            
+    
+    
+    
+   
+    
+
+
         
         
     
