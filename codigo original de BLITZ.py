6255407bc601ad74c7a95367a0360ea3fe348e47
Syr-Galahad <107660821+Syr-Galahad@users.noreply.github.com>
2022-07-26 19:47:26 -0500

    Add files via upload

---
+++ codigo original de BLITZ.py
@@ -0,0 +1,266 @@
+"""Videogame made by Carlos G and Jair D"""
+
+
+from shutil import move
+from turtle import update
+import pygame, sys, time, random
+from tkinter import*
+import serial, time
+
+arduino = serial.Serial("COM4", 9600)
+
+pygame.init()
+
+#just colors 
+white=(255,255,255)
+black=(0,0,0)
+red=(255,0,0)
+#Classic variables 
+screen = pygame.display.set_mode((1000, 900))
+font = pygame.font.Font(None, 30)
+clock = pygame.time.Clock()
+
+pygame.mixer.music.load("Soundtrack.mp3")
+pygame.mixer.music.play()
+pygame.mixer.music.set_volume(0.3)
+
+#Make the fondo and heart 
+Space=pygame.image.load("Fondo.jpg")
+pygame.mouse.set_visible(0)
+Heart=pygame.image.load("heart.png")
+
+
+#coordenadas del corazon
+coordx=450
+coordy=450
+#fps
+speedx= 0
+speedy= 0
+speedN = -5
+modegame = "NICE"
+class Meteor(pygame.sprite.Sprite):
+	def __init__(self):
+		super().__init__()
+		self.image = pygame.image.load("enemey.png").convert()
+		self.image.set_colorkey(black)
+		self.rect = self.image.get_rect()
+
+	def update(self):
+		
+		if modegame == "EASY":
+			self.rect.x -= 5
+		if modegame == "NORMAL":
+			self.rect.x -= 10
+		if modegame == "HARD":
+			self.rect.x -= 15
+		if modegame == "TRYHARD":
+			self.rect.x -= 20
+		else:
+			self.rect.x -=1
+        
+  
+
+		if self.rect.x < 0:
+			self.rect.x = +900
+			self.rect.y = random.randrange(900)
+class Player(pygame.sprite.Sprite):
+	def __init__(self):
+		super().__init__()
+		self.image = pygame.image.load("heart.png").convert()
+		self.image.set_colorkey(white)
+		self.rect = self.image.get_rect()
+
+	def update(self):
+		player.rect.x = coordx
+		player.rect.y = coordy
+
+run = True
+timer =  0
+realtime =0
+dt = 1
+ap = 0
+press = "Press F to End"
+Lifes = 5
+vida = " Vidas"
+endless = False
+
+#call the functions by a name 
+pikes = update
+board = [0,10]
+meteor_list = pygame.sprite.Group()
+all_sprite_list = pygame.sprite.Group()
+player = Player()  
+all_sprite_list.add(player)
+
+Sound=pygame.mixer.Sound("Oof.mp3")
+Sound.set_volume(0.7)
+Final=pygame.mixer.Sound("Finally.mp3")
+Final.set_volume(0.8)
+
+for i in range(7):
+	meteor = Meteor()
+	meteor.rect.x = random.randrange(800,1300)
+	meteor.rect.y = random.randrange(300,800)
+	meteor_list.add(meteor)
+	all_sprite_list.add(meteor)
+while run:
+        #timer 
+        pygame.init()
+        randomColor = random.randint(0,25)*10
+        timer += dt
+        clock.tick(60)
+        all_sprite_list.update()
+        
+        #to make the conter
+        if timer>60 :
+            realtime+=1
+            timer = 0
+        #condition objects based in time
+        if realtime< 15 and realtime>-1:
+            screen.fill((0,0,0))
+            modegame = "EASY"  
+        if realtime >= 15 and realtime<30:
+            
+            screen.fill((0,0,0))
+            modegame = "NORMAL"  
+        if realtime >= 30 and realtime<50:
+            screen.fill((ap,0,0))
+            modegame = "HARD"
+        if realtime >= 50 and realtime<60:
+            screen.fill((ap,0,0))
+            modegame = "TRYHARD"
+        if realtime == 60 :
+            meteor.rect.x = 450
+            realtime = 61
+            modegame = "YOU WON"  
+       #to restore the position every time 
+        if realtime >=65:
+            run = False
+            
+         #Movement when you want in keyboard 
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+            
+             if event.key== pygame.K_LEFT:
+                speedx=-5
+             if event.key==pygame.K_RIGHT:
+                speedx= 5
+             if event.key==pygame.K_UP:
+                speedy=-5
+             if event.key==pygame.K_DOWN:
+                speedy= 5
+             if event.key == pygame.K_f: #Just if you want to fail 
+                    modegame = "You have failed!"
+                    realtime = 61
+                    print("You has surrendered!")
+
+        x = str(arduino.read().strip()).strip('b')
+        #to move around in bottons 
+        if x == "'1'":
+            coordy = 700
+        if x == "'2'":
+            coordy= 200
+        if x == "'3'":
+            speedy= 10
+        if x == "'4'":
+            speedy= -10
+          if x == "5":
+            speedx=10
+        if x == "6":
+            speedx=-10    
+        if x == "'0'":
+            speedy = 0
+            speedx =0
+                
+            
+        #When the heart hit the boarders 
+        if coordx >= 900:
+            speedx = -10
+        if coordx <= 0:
+            speedx = 10
+        if coordy <= 150:
+            speedy = 10
+        if coordy >= 750:
+            speedy =-10
+        #The special movement 
+        coordx+=speedx
+        coordy+=speedy   
+        
+        meteor_hit_list = pygame.sprite.spritecollide(player, meteor_list, True)
+        for meteor in meteor_hit_list:
+            if Lifes <= 0:
+                realtime = 61
+                modegame = "Game Over"
+                run == False
+                
+                
+            if Lifes ==0:
+                Sound.set_volume(0.0)
+                Final.play()
+                pygame.mixer.music.set_volume(0.1)
+                
+            Lifes = Lifes -1
+            print("You have failed !")
+            Sound.play()
+            
+            
+        #Close when the clock reach 65 seg
+        
+
+       
+                
+         
+        
+    
+         
+        
+        #Print the objects 
+        screen.blit(Space,[0,0])
+        text = font.render(str(realtime),0,(200,60,80))
+        end = font.render(str(press),0,(200,60,80))
+        textcenter = font.render(str(modegame),0,(200,60,80))
+        word = font.render(str(vida),0,(200,60,80))
+        screen.blit(textcenter,(450,100))
+        screen.blit(text, (850,100))
+        screen.blit(end,(10,100))
+        hearts = font.render(str(Lifes),0,(200,60,80))
+        screen.blit(hearts,(200,100))
+        screen.blit(word,(210,100))
+        all_sprite_list.draw(screen)
+        
+        pygame.display.flip()
+
+
+
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+           
+            
+    
+    
+    
+   
+    
+
+
+        
+        
+    
---
+++ codigo_de_BLITZ.ino
@@ -0,0 +1,63 @@
+//Carlos Jesus Garcia Cano 20/07/2022
+const int but1 = 5;
+const int  but2 = 6;
+const int but3 = 4;
+const int but4 = 3;
+const int but5 = 2;
+const int but6 =1;
+void setup() {
+//a
+  Serial.begin(9600);
+  pinMode(but1,INPUT);
+  pinMode(but2,INPUT);
+  pinMode(but3,INPUT);
+  pinMode(but4,INPUT);
+  pinMode(but5,INPUT);
+  pinMode(but6,INPUT);
+  Serial.println(0);  
+
+}
+
+void loop() {
+  if(digitalRead(but1) == HIGH)
+  {
+    Serial.println(1);
+    digitalWrite(but1,LOW) ;
+    delay(50);
+    }
+  if(digitalRead(but2) == HIGH)
+  {
+    Serial.println(2);
+    digitalWrite(but2,LOW) ;
+    delay(50);
+    }
+  if(digitalRead(but3) == HIGH)
+  {
+    Serial.println(3);
+    digitalWrite(but3,LOW) ;
+    delay(50);
+    }
+  if(digitalRead(but4) == HIGH)
+  {
+    Serial.println(4);
+    digitalWrite(but4,LOW) ;
+    delay(50);
+    }
+  if(digitalRead(but5)==HIGH)
+  {
+    Serial.println(5);
+    digitalWrite(but5,LOW);
+    delay(50);
+    }
+  if(digitalRead(but6)==HIGH)
+  {
+    Serial.println(6);
+    digitalWrite(but6,LOW);
+    delay(50);
+    }  
+  else
+  {
+    Serial.println(0);
+    delay(50);
+   }
+}
